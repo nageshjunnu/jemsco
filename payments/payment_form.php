59a642b7a149710d27126d4c2f4cbc2bda44af3f
Nagesh <nageshalwaystogether@gmail.com>
2023-09-13 02:09:59 +0530

    updated the code

--- payments/payment_form.php
+++ payments/payment_form.php
@@ -267,89 +267,7 @@ button:hover{
 			</div>
 		</header>
 		<!-- HEADER : end -->
-		<section>
-        <div class="content">
-
-            <div class="basic box">
-                <h2 class="title">Basic</h2>
-                <div class="view">
-                    <div class="icon">
-                        <img src="https://i.postimg.cc/2jcfMcf4/hot-air-balloon.png" alt="hot-air-balloon">
-                    </div>
-                    <div class="cost">
-                        <p class="amount">$09</p>
-                        <p class="detail">per student per month</p>
-                    </div>
-                </div>
-                <div class="description">
-                    <ul>
-                        <li>Lorem, ipsum dolor.</li>
-                        <li>Harum, beatae laudantium.</li>
-                        <li>Odit, fugit saepe.</li>
-                        <li>Harum, veniam suscipit!</li>
-                        <li>A, aut veritatis!</li>
-                        <li>Aliquid, quasi repellat!</li>
-                    </ul>
-                </div>
-                <div class="button">
-                    <button type="submit" >START FREE 7 DAYS TRIAL</button>
-                </div>
-            </div>
-    
-            <div class="standard box">
-                <h2 class="title">Standard</h2>
-                <div class="view">
-                    <div class="icon">
-                        <img src="https://i.postimg.cc/DzrTN72Z/airplane.png" alt="airplane">
-                    </div>
-                    <div class="cost">
-                        <p class="amount">$99</p>
-                        <p class="detail">per student per year</p>
-                    </div>
-                </div>
-                <div class="description">
-                    <ul>
-                        <li>Lorem, ipsum dolor.</li>
-                        <li>Harum, beatae laudantium.</li>
-                        <li>Odit, fugit saepe.</li>
-                        <li>Harum, veniam suscipit!</li>
-                        <li>A, aut veritatis!</li>
-                        <li>Aliquid, quasi repellat!</li>
-                    </ul>
-                </div>
-                <div class="button">
-                    <button type="submit" >START FREE 7 DAYS TRIAL</button>
-                </div>
-            </div>
-    
-            <div class="business box">
-                <h2 class="title">Business</h2>
-                <div class="view">
-                    <div class="icon">
-                        <img src="https://i.postimg.cc/wvFd6FRY/startup.png" alt="startup">
-                    </div>
-                    <div class="cost">
-                        <p class="amount">$199</p>
-                        <p class="detail">per team per year</p>
-                    </div>
-                </div>
-                <div class="description">
-                    <ul>
-                        <li>Lorem, ipsum dolor.</li>
-                        <li>Harum, beatae laudantium.</li>
-                        <li>Odit, fugit saepe.</li>
-                        <li>Harum, veniam suscipit!</li>
-                        <li>A, aut veritatis!</li>
-                        <li>Aliquid, quasi repellat!</li>
-                    </ul>
-                </div>
-                <div class="button">
-                    <button type="submit" >START FREE 7 DAYS TRIAL</button>
-                </div>
-            </div>
-
-        </div>
-    </section>
+	
 
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
@@ -366,7 +284,7 @@ button:hover{
             <!-- Add other necessary form fields -->
 			<div class="content">
             <div class="standard box">
-                <h2 class="title">Standard</h2>
+                <h2 class="title">Payment</h2>
                 <div class="view">
                     
                     <div class="cost">
