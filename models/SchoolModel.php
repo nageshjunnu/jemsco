0d1f9555cb364971740f9685037defcf0b3db4d4
Nagesh <nageshalwaystogether@gmail.com>
2023-09-14 00:41:17 +0530

    student registration

--- models/SchoolModel.php
+++ models/SchoolModel.php
@@ -9,7 +9,7 @@ class SchoolModel {
         $con = new dbModel();
         $connection = $con->conn();
         
-        $query = "INSERT INTO schools (name, address, city,district, state, pincode, is_trust_society, gst, phone, alternate_phone, email, alternate_email, principal, principal_phone, principal_email, co_ordinator_name,co_ordinator_phone, co_ordinator_email, board, catalyst_olympiad, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $query = "INSERT INTO schools (name, address, city,district, state, pincode, is_trust_society, gst, phone, alternate_phone, email, alternate_email, principal, principal_phone, principal_email, co_ordinator_name,co_ordinator_phone, co_ordinator_email, board, catalyst_olympiad, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
            
         $stmt = $connection->prepare($query);
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
