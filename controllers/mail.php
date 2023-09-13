e77ec0f2b1794337c5a8005041007f5eee3fcedf
Nagesh <nageshalwaystogether@gmail.com>
2023-09-13 15:54:58 +0530

    updated code

--- controllers/mail.php
+++ controllers/mail.php
@@ -1,80 +1,64 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\SMTP;
-// use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-// require 'vendor/autoload.php'; // Include the PHPMailer autoloader
+require 'vendor/autoload.php'; // Include the PHPMailer autoloader
 
-// $mail = new PHPMailer(true);
+$mail = new PHPMailer(true);
 
-// try {
-//     // Server settings
-//     $mail->isSMTP();
-//     $mail->SMTPAuth   = true;
-//     $mail->SMTPSecure = 'tls';
-//     $mail->Host       = 'smtp.gmail.com'; // Replace with your SMTP server
-//     $mail->Port       = 587; // Replace with your SMTP port
-//     $mail->Username   = 'nagesh@orchids.edu.in';
-//     $mail->Password   = 'Junnu_2023';
+try {
+    // Server settings
+    $mail->isSMTP();
+    $mail->SMTPAuth   = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->Host       = 'smtp.gmail.com'; // Replace with your SMTP server
+    $mail->Port       = 587; // Replace with your SMTP port
+    $mail->Username   = 'nagesh@orchids.edu.in';
+    $mail->Password   = 'Junnu_2023';
 
-//     // $mail->SMTPOptions = array(
-//     //     'ssl' => array(
-//     //         'verify_peer' => false,
-//     //         'verify_peer_name' => false,
-//     //         'allow_self_signed' => true
-//     //     )
-//     // );
-//     // $mail->SMTPDebug = 2;
+    // $mail->SMTPOptions = array(
+    //     'ssl' => array(
+    //         'verify_peer' => false,
+    //         'verify_peer_name' => false,
+    //         'allow_self_signed' => true
+    //     )
+    // );
+    // $mail->SMTPDebug = 2;
     
-//     // Sender and recipient
-//     $mail->setFrom('jemsco2023@gmail.com', 'JEMSCO 2023');
-//     $mail->addAddress('nageshy.php@gmail.com', 'Nagesh');
+    // Sender and recipient
+    $mail->setFrom('jemsco2023@gmail.com', 'JEMSCO 2023');
+    $mail->addAddress('nageshy.php@gmail.com', 'Nagesh');
 
-//     // Email content
-//     $mail->isHTML(true);
-//     $mail->Subject = 'Student Registration Success';
-//     $mail->Body    = "<!DOCTYPE html>            <html>
-//     <head>
-//         <title>Student Registration Success</title>
-//     </head>
-//     <body>
-//         <h1>Welcome, Nagesh!</h1>
-//         <p>Your registration for CSO,CEO has been successfully processed.</p>
-//         <p>Thank you for choosing our institution!</p>
-//     </body>
-//     </html>            "; // Load your email template file
+    // Email content
+    $mail->isHTML(true);
+    $mail->Subject = 'Student Registration Success';
+    $mail->Body    = "<!DOCTYPE html>            <html>
+    <head>
+        <title>Student Registration Success</title>
+    </head>
+    <body>
+        <h1>Welcome, Nagesh!</h1>
+        <p>Your registration for CSO,CEO has been successfully processed.</p>
+        <p>Thank you for choosing our institution!</p>
+    </body>
+    </html>            "; // Load your email template file
 
-//     // Replace placeholders in the email template
-//     $studentName = 'John Doe'; // Replace with the actual student name
-//     $courseName = 'Computer Science'; // Replace with the actual course name
-//     $mail->Body = str_replace('John', $studentName, $mail->Body);
-//     $mail->Body = str_replace('CEO', $courseName, $mail->Body);
-//     $mail->SMTPDebug = SMTP::DEBUG_CONNECTION; // or DEBUG_SERVER for more detailed information
-//     $mail->Debugoutput = 'error_log'; // or 'echo' to display in the browser
-//     $mail->send();
-//     echo "success";
-//     return true;
-// } catch (Exception $e) {
-//      echo "Email could not be sent. Error: {$mail->ErrorInfo}";
-//     //return false
-// }
-
-$to = "nageshy.php@gmail.com";//change receiver address  
-$subject = "This is subject";  
-$message = "<h1>This is HTML heading</h1>";  
-
-$header = "From:nageshalwaystogether@gmail.com \r\n";  
-$header .= "MIME-Version: 1.0 \r\n";  
-$header .= "Content-type: text/html;charset=UTF-8 \r\n";  
-
-$result = mail ($to,$subject,$message,$header);  
-
-if( $result == true ){  
-   echo "Message sent successfully...";  
-}else{  
-   echo "Sorry, unable to send mail...";  
-}  
+    // Replace placeholders in the email template
+    $studentName = 'John Doe'; // Replace with the actual student name
+    $courseName = 'Computer Science'; // Replace with the actual course name
+    $mail->Body = str_replace('John', $studentName, $mail->Body);
+    $mail->Body = str_replace('CEO', $courseName, $mail->Body);
+    $mail->SMTPDebug = SMTP::DEBUG_CONNECTION; // or DEBUG_SERVER for more detailed information
+    $mail->Debugoutput = 'error_log'; // or 'echo' to display in the browser
+    $mail->send();
+    echo "success";
+    return true;
+} catch (Exception $e) {
+     echo "Email could not be sent. Error: {$mail->ErrorInfo}";
+    //return false
+}
 
 ?>
