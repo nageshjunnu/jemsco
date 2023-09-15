1c16a32822fa1869c3ec23983cb7b0b036000ca2
Nagesh <nageshalwaystogether@gmail.com>
2023-09-16 02:51:54 +0530

    fixing issues

--- views/all-users.php
+++ views/all-users.php
@@ -133,7 +133,7 @@ $usersdata = $usersModel->getAllUsers();
                 if (confirm('Are you sure you want to delete this user?')) {
                     $.ajax({
                         type: 'POST',
-                        url: 'controllers/UserController.php',
+                        url: '../controllers/UserController.php',
                         data: { user_id: userId, action:"delete-user" },
                         dataType: 'json',
                         success: function(response) {
