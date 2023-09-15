95632b64c95a5e78fbe2e81bccd04d059620e2ea
Nagesh <nageshalwaystogether@gmail.com>
2023-09-15 11:47:27 +0530

    redirection issue fixed

--- views/footer.php
+++ views/footer.php
@@ -122,24 +122,16 @@
 		
 		
 
-	<!-- Page Content overlay -->
-	<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-
-	
-	<!-- Vendor JS -->
-	<script src="assets/src/js/vendors.min.js"></script>
-	<!-- <script src="assets/src/js/pages/chat-popup.js"></script> -->
-    <script src="assets/icons/feather-icons/feather.min.js"></script>
-	
-	<!-- <script src="assets/assets/vendor_components/apexcharts-bundle/dist/apexcharts.js"></script> -->
-	
+		<!-- Vendor JS -->
+        <script src="assets/src/js/vendors.min.js"></script>
+	<script src="assets/src/js/pages/chat-popup.js"></script>
+    <script src="assets/assets/icons/feather-icons/feather.min.js"></script>
 	<script src="assets/vendor_components/datatable/datatables.min.js"></script>
 
 	
-	<!-- JEMSCO App -->
-	<!-- <script src="assets/src/js/template.js"></script> -->
-	<script src="assets/src/js/pages/dashboard.js"></script>
-    <script src="assets/src/js/pages/data-table.js"></script>
+	<!-- CRMi App -->
+	<script src="assets/src/js/template.js"></script>
+	<script src="assets/src/js/pages/data-table.js"></script>
 
 	
 </body>
