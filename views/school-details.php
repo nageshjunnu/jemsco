cdc4b15d43147b85a5386b44b7cb241e61fee944
Nagesh <nageshalwaystogether@gmail.com>
2023-09-16 18:36:53 +0530

    fixing issues

--- views/school-details.php
+++ views/school-details.php
@@ -122,6 +122,7 @@ $schoolDetails = $schoolController->getSchoolDetailsById($_GET['id']);
 							</div>
 						
 					</div>
+                    </div>
 		                <div class="row">					
 						<div class="col-lg-6 col-md-12">
 							<div class="box">
