edf6d0d9fe3f044e63d52605e64db504c68ebfe4
Nagesh <nageshalwaystogether@gmail.com>
2023-09-16 21:59:23 +0530

    fixing issues

--- views/schools.php
+++ views/schools.php
@@ -1,7 +1,7 @@
 <?php
 // session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+// error_reporting(E_ALL);
+// ini_set('display_errors', '1');
 require_once '../models/UserModel.php';
 require_once '../controllers/schoolsControllers.php';
 
